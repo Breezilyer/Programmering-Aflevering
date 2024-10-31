8c67ea1562e681f621e656562e4180e103f12dec
Breezilyer <emilpeter22@gmail.com>
2024-10-31 22:10:44 +0100

    test

--- Aflevering1/Program.cs
+++ Aflevering1/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace Aflevering1
+﻿namespace Aflevering1;
 using static System.Console; // Den her linje er for at man ikke behøver at skrive "Console" heletiden, så du initialisere det på hele projekted
-{
     internal class Program
 {
     // her indsætter jeg de tabeller med varePris og vareNavn. Der skal være 10 hver som opgave siger.
     static int[] itemPrice = new int[10];
-    static string[] itemPrice = new string[10];
+    static string[] itemName = new string[10];
 
     // Her indsætter jeg admin login, for at man kan login og indsætter vare og priser.
     static string admin = "admin";
@@ -13,16 +12,16 @@ using static System.Console; // Den her linje er for at man ikke behøver at skr
 
     static void Main(string[] args)
     {
-
+        ReadKey();
     }
 
-    static int menu()
+    static void menu()
     {
         string[] options = { "Login", "Add item", "Show list", "Order item", "Exit" };
         for (int i = 0; i < options.Length; i++)
         {
             WriteLine($"{i + 1}: {options[i]}");
         }
+        
     }
-}
 }
